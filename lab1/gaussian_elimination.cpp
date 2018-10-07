69609af68bd977a8db4ffa92df33d1fc6cbf5cce
jakub <kuba.koniecznyxx@gmail.com>
2018-10-07 13:03:42 +0200

    initial commit

---
+++ lab1/gaussian_elimination.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+using namespace std;
+typedef double number;
+
+//  h := 1 /* Initialization of the pivot row */
+//  k := 1 /* Initialization of the pivot column */
+//  while h ≤ m and k ≤ n
+//    /* Find the k-th pivot: */
+//    i_max := argmax (i = h ... m, abs(A[i, k]))
+//    if A[i_max, k] = 0
+//      /* No pivot in this column, pass to next column */
+//      k := k+1
+//    else
+//       swap rows(h, i_max)
+//       /* Do for all rows below pivot: */
+//       for i = h + 1 ... m:
+//          f := A[i, k] / A[h, k]
+//          /* Fill with zeros the lower part of pivot column: */
+//          A[i, k]  := 0
+//          /* Do for all remaining elements in current row: */
+//          for j = k + 1 ... n:
+//             A[i, j] := A[i, j] - A[h, j] * f
+//       /* Increase pivot row and column */
+//       h := h+1 
+//       k := k+1
+
+number absolute(number x){
+    return x < 0 ? (-1)*x : x;
+}
+
+void swap_rows(int n, number ** A, int row1, int row2){
+    number Tmp[n+1];
+
+    for(int i = 0; i <= n; i++){
+        Tmp[i] = A[row1][i];
+        A[row1][i] = A[row2][i];
+        A[row2][i] = Tmp[i];
+    }
+}
+
+void elimination(int n, number ** A, number * X){
+/*
+    number a;
+
+    //dla kazdej kolumny oprocz ostatniej
+    for(int i = 0; i < n-1; i++){
+
+        //jesli liczba na przekatnej nie jest zerem (jesli jest to nie da sie policzyc)
+        if(A[i][i] == 0){
+            printf("LOL\n");
+            exit(1);
+        }
+
+        //dla kazdego wiersza o numerze wiekszym niz nr rozpatrywanej kolumny
+        for(int j = i+1; j < n; j++){
+
+
+            //wylicz mnoznik dla wiersza do dodania
+            a = -A[j][i] / A[i][i];
+
+            //dodaj przemnozony wiersz i
+            for(int k = i + 1; k < n; k++)
+                A[j][k] += a * A[i][k];
+        }
+    }
+
+    //wyliczanie niewiadomych
+    for(int i = n - 1; i >= 0; i--){
+
+        a = A[i][n];
+
+        for(int j = n - 1; j >= i + 1; j--){
+            a -= A[i][j] * X[j];
+        }
+
+        if(A[i][i] == 0){
+            printf("LOL2");
+            exit(1);
+        }
+        X[i] = a/A[i][i];
+    }
+
+
+    cout<<"XD"<<endl;
+*/
+
+    int row = 0; //nr rzedu pivotu
+    int column = 0; //nr kolumny pivtu
+    while(row < n and column <= n){
+
+        //znajdywanie najwiekszej wartosci w ktej kolumnie w pozostalych wierszach i indeksu wiersza
+        number maximal_value_in_kth_column = 0;
+        int index = row;
+
+        for(int i = row; i < n; i++){
+            number tmp = absolute(A[i][column]);
+
+            if(maximal_value_in_kth_column < tmp){
+                maximal_value_in_kth_column = tmp;
+                index = i;
+            }
+        }
+
+        if(maximal_value_in_kth_column == 0){
+            //wszystkie wiersze w tej kolumnie maja wartosc 0
+            column++;
+        }
+        else{
+            //wrzucamu wiersz z najwieksza wartoscia na gore - zeby nie mial 0
+            swap_rows(n, A, row, index);
+
+            //dla pozostalych wierszy
+            for(int i = row + 1; i < n; i++){
+                number factor = A[i][column] / A[row][column];
+
+                //odejmujemy odpowiedno przemnozony rzad pivot
+                A[i][column] = 0;
+                for(int j = column + 1; j <= n; j++){
+                    A[i][j] = A[i][j] - factor * A[row][j];
+                }
+            }
+         
+            row++;
+            column++;
+        }
+    }
+
+    cout<<"XD"<<endl;
+
+    //wyliczanie niewiadomych
+    for(int i = n - 1; i >= 0; i--){
+
+        int s = A[i][n];
+        for(int j = n - 1; j > i; j--){ //popraw warunek konca
+            s -= A[i][j] * X[j];
+        }
+        if(A[i][i] == 0){
+            cout<<"LOOOOL"<<endl;
+            exit(1);
+        }
+        X[i] = s / A[i][i];
+    }
+
+    cout<<"XD2"<<endl;
+}
+
+int main(){
+
+    number **A;
+    number *X;
+    int n;
+
+    cout << "wpisz liczbe niewiadomych" << endl;
+
+    cin >> n;
+
+    A = new number*[n];
+    for(int i = 0; i < n; i++)
+        A[i] = new number[n + 1];
+
+    X = new number[n];
+
+    printf("wpisz kolejno wiersze tablicy\n");
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    printf("wpisz wartosci wektora prawej strony\n");
+
+    for(int i = 0; i < n; i++)
+        cin >> A[i][n];
+
+    //wypisz
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << A[i][j] << " ";
+        }
+        cout << "| x" << i << " |  " << A[i][n] << endl;
+    }
+
+
+    elimination(n, A, X);
+
+
+    //wypsiz
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << A[i][j] << " ";
+        }
+        cout << "| " << X[i] << " |  " << A[i][n] << endl;
+    }
+
+
+}
+
+
