f9f8186bae8381290f7cd478dfa3ea81bd967ea0
jakub <kuba.koniecznyxx@gmail.com>
2018-11-03 11:58:42 +0100

    added 1st version of task 1

--- lab2/jacobi_method.cpp
+++ lab2/jacobi_method.cpp
@@ -5,8 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 using namespace std;
-typedef float number;
+typedef double number;
 
 
 number absolute(number x){
@@ -67,73 +68,9 @@ number maximum_norm(int n, number * X){
     return max_number;
 }
 
-number * jacobi_method(int n, number ** A, int number_of_iterations){
-    //alokacja i wypelnienie poczatkowego wektora niewiadomych X
-    number * X = new number[n];
-    for(int i = 0; i < n; i++)
-        X[i] = 0;
-
-    //odwrocenie wartosci na przekatnej macierzy - traktujemy wtedy te przekatna jako N = D ^ (-1)
-    for(int i = 0; i < n; i++){
-        A[i][i] = 1 / A[i][i];
-    }
-
-    //obliczenie wartości N*b, gdzie b to wektor prawej strony
-    number * Nb = new number[n];
-    for(int i = 0; i < n; i++){
-        Nb[i] = A[i][i] * A[i][n];
-    }
-
-    //obliczenie macierzy M = -N * (L + U)
-    number ** M = new number * [n];
-    for(int i = 0; i < n; i++){
-        M[i] = new number[n];
-
-        for(int j = 0; j < n; j++){
-
-            M[i][j] = 0;
-            if(i != j) 
-                M[i][j] -= A[i][i] * A[i][j];
-        }
-    }
-
-    //wykonanie number_of_iterations iteracji przyblizania wektora X
-    for(int iteration = 0; iteration < number_of_iterations; iteration++){
-
-        number * newX = new number[n];
-        for(int i = 0; i < n; i++){
-            newX[i] = Nb[i];
-            for(int j = 0; j < n; j++){
-                newX[i] += (M[i][j] * X[j]);
-            }
-        }
-
-        for(int i = 0; i < n; i++){
-            X[i] = newX[i];
-        }
-
-        // cout<<endl;
-        // print_vector(n, X);        
-    }
-
-    return X;
-}
-
-
-
-//eksperyment jak na 1 labach
-
-void multiply(int n, number ** A, number * X, number * B){
-    for(int i = 0; i < n; i++){
-        B[i] = 0;
-        for(int j = 0; j < n; j++){
-            B[i] += A[i][j] * X[j];
-        }
-    }
-}
-
 void print_vector_and_norms(int n, number * X, number * X2){
     
+    cout << "\nwyliczony wektor niewiadomych" << endl;    
     print_vector(n, X2);
 
     cout << "\nnorma euklidesowa wektora zadanego " << euclidean_norm(n, X) << endl;
@@ -146,7 +83,124 @@ void print_vector_and_norms(int n, number * X, number * X2){
 
 }
 
-void experiment(int n, number ** A, int number_of_iterations){
+void multiply(int n, number ** A, number * X, number * B){
+    for(int i = 0; i < n; i++){
+        B[i] = 0;
+        for(int j = 0; j < n; j++){
+            B[i] += A[i][j] * X[j];
+        }
+    }
+}
+
+bool warunek_stopu_1(int n, number * X, number * newX, number ro){
+
+    number * diffX = new number[n];
+
+    for(int i = 0; i < n; i++){
+        diffX[i] = newX[i] - X[i];
+    }
+
+    if(maximum_norm(n, diffX) < ro)
+        return true;
+    else
+        return false;
+}
+
+bool warunek_stopu_2(int n, number * newX, number ** A, number ro){
+
+    number * B = new number[n];
+    number * diffB = new number[n];
+
+    multiply(n, A, newX, B);    
+
+    for(int i = 0; i < n; i++){
+        diffB[i] = B[i] - A[i][n];
+    }
+
+    cout<<"wyliczony wektor"<<endl;
+    print_vector(n, B);
+    cout << "roznica " << maximum_norm(n, diffB) << endl;
+sleep(1);
+    if(maximum_norm(n, diffB) < ro)
+        return true;
+    else
+        return false;
+}
+
+number * jacobi_method(int n, number ** A, number ro){
+    //alokacja i wypelnienie poczatkowego wektora niewiadomych X
+    number * X = new number[n];
+    for(int i = 0; i < n; i++)
+        X[i] = 0;
+
+    number ** copyA = new number * [n];
+    for(int i = 0; i < n; i++){
+        copyA[i] = new number [n+1];
+        for(int j = 0; j < n+1; j++){
+            copyA[i][j] = A[i][j];
+        }
+    }
+
+    //odwrocenie wartosci na przekatnej macierzy - traktujemy wtedy ta przekatna jako N = D ^ (-1)
+    for(int i = 0; i < n; i++){
+        A[i][i] = 1 / A[i][i];
+    }
+
+    //obliczenie wartości N*b, gdzie b to wektor prawej strony
+    number * Nb = new number[n];
+    for(int i = 0; i < n; i++){
+        Nb[i] = A[i][i] * A[i][n];
+    }
+
+    //obliczenie macierzy iteracji M = -N * (L + U)
+    number ** M = new number * [n];
+    for(int i = 0; i < n; i++){
+        M[i] = new number[n];
+
+        for(int j = 0; j < n; j++){
+
+            M[i][j] = 0;
+            if(i != j) 
+                M[i][j] -= A[i][i] * A[i][j];
+        }
+    }
+
+    // cout<<"\nmacierz iteracji"<<endl;
+    // print_matrix(n, M);
+    
+    //alokacja nowego wektora X i zmiennej przechowujacej liczbe iteracji
+    number * newX = new number[n];
+    int number_of_iterations = 0;
+
+    //wykonywanie iteracji dopoki nie zajdzie warunek stopu
+    while(true){
+        number_of_iterations++;
+
+        //obliczenie nowego wektora X
+        for(int i = 0; i < n; i++){
+            newX[i] = Nb[i];
+            for(int j = 0; j < n; j++){
+                newX[i] += (M[i][j] * X[j]);
+            }
+        }
+
+        // if(warunek_stopu_1(n, X, newX, ro))
+        //     break;
+
+        if(warunek_stopu_2(n, newX, copyA, ro))
+            break;
+
+        for(int i = 0; i < n; i++){
+            X[i] = newX[i];
+        }    
+    }
+
+    cout << "\nliczba iteracji " << number_of_iterations << endl;
+
+    return newX;
+}
+
+void experiment(int n, number ** A, number ro){
     //stworzenie i wypelnienie wektora niewiadomych
     number * X = new number[n];
     fill_vector_X(n, X);
@@ -161,108 +215,59 @@ void experiment(int n, number ** A, int number_of_iterations){
         A[i][n] = B[i];
     }
 
-    cout << "\nwektor prawej strony - b" << endl;
-    print_vector(n, B);
-
     cout << "\nmacierz" << endl;
     print_matrix(n, A);
 
     number * X2 = new number[n];
-    X2 = jacobi_method(n, A, number_of_iterations);
+    X2 = jacobi_method(n, A, ro);
 
-    cout << "\nwyliczony wektor niewiadomych" << endl;    
     print_vector_and_norms(n, X, X2);
 
 }
 
-//koniec eksperymentu
+void zad1(int n, number ro){
 
-/*
-void gaussian_elimination(int n, number ** A){
-
-    int row = 0; //nr rzedu pivotu
-    int column = 0; //nr kolumny pivtu
-    while(row < n and column <= n){
-
-        //znajdywanie najwiekszej wartosci w kolumnie pivocie w pozostalych wierszach
-        number max_value = 0;
-        int max_value_row = row;
-
-        for(int i = row; i < n; i++){
-
-            number tmp = absolute(A[i][column]);
-
-            if(max_value < tmp){
-                max_value = tmp;
-                max_value_row = i;
-            }
-        }
-
-        if(max_value == 0){
-            //wszystkie wiersze w tej kolumnie maja wartosc 0
-            column++;
-        }
-        else{
-            //jesli w wierszu pivocie mamy 0 w kolumnie pivocie to podmieniamy rzad
-            if(A[row][column] == 0)
-                swap_rows(n, A, row, max_value_row);
-
-            //dla pozostalych wierszy odejmujemy odpowiedno przemnozony rzad pivot
-            for(int i = row + 1; i < n; i++){
-                number factor = A[i][column] / A[row][column];
-
-                A[i][column] = 0;
-                for(int j = column + 1; j <= n; j++){
-                    A[i][j] = A[i][j] - factor * A[row][j];
-                }
-            }
-         
-            row++;
-            column++;
-        }
-    }
-}
-
-
-void calculate_variables(int n, number ** A, number * X){
-    for(int i = n - 1; i >= 0; i--){
-
-        number s = A[i][n];
-        for(int j = n - 1; j > i; j--){
-            s -= A[i][j] * X[j];
-        }
-        if(A[i][i] == 0){
-            cout << "\nZero na przekątnej - uklad nie ma dokladnie 1 rozwiazania" << endl;
-            exit(0);
-        }
-        X[i] = s / A[i][i];
-    }
-}
-
-*/
-
-void zad1(int n, int number_of_iterations){
-    
+    //alokacja macierzy A    
     number ** A = new number * [n];
     for(int i = 0; i < n; i++){
         A[i] = new number[n+1];
     }
 
-    cout << "wpisz macierz rozszerzona" << endl;
+    //wypelnienie macierzy A
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n + 1; j++){
-            cin >> A[i][j];
+        //wypelnienie prezkatnej
+        A[i][i] = 5;
+    
+        //wypelnienie pol po lewej od przekatnej
+        if(i != 0){
+            A[i][i-1] = 0.25 / ((number) i + 1);
+        }
+
+        //wypelnienie pol nad przekatna
+        for(int j = i + 1; j < n; j++){
+            if((j + 1) % 2 == 0)
+                A[i][j] = 0.25 / ((number) j + 1);
+            else
+                A[i][j] = (-1) * 0.25 / ((number) j + 1);
+        }
+
+        //wypelnienie pozostalych pol
+        if(i >= 2){
+            for(int j = i - 2; j >= 0; j--){
+                A[i][j] = 0;
+            }
         }
     }
 
-    experiment(n, A, number_of_iterations);
+    experiment(n, A, ro);
 }
 
 int main(){
-    int n, number_of_iterations;
-    cout << "wpisz rozmiar macierzy i liczbe iteracji" << endl;
-    cin >> n >> number_of_iterations;
+    int n;
+    number ro;
+    cout << "wpisz rozmiar macierzy i ro z warunku stopu" << endl;
+    cin >> n >> ro;
 
-    zad1(n, number_of_iterations);
+    zad1(n, ro);
 
 }
