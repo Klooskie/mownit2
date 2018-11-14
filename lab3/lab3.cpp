bc7673cf5b9a205025f27e734b4c9424b3b632cc
jakub <kuba.koniecznyxx@gmail.com>
2018-11-14 12:45:17 +0100

    after classes

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -10,48 +10,58 @@ using namespace std;
 typedef double number;
 
 
+number proper_zero_of_f = 0.522247;
+
 number absolute(number x){
     return x < 0 ? (-1) * x : x;
 }
 
-//f(x) = x^2 - 9
+//f(x) = (x - 1) * e ^ (-16 * x) + x ^ 14
+//f(x) = 0 dla x = 0.522247
 number f_function(number x){
-    return x * x - 9;
+
+    number f = exp((-16) * x);
+    f *= (x - 1);
+    f += pow(x, 14);
+    return f;
+    
 }
 
-//f'(x) = 2 * x
+//f'(x) = 14 * x ^ 13 + e ^ (-16x) (17 - 16x)
 number f_derivative(number x){
-    return 2 * x;
+    
+    number f = exp((-16) * x);
+    f *= (17 - (16 * x));
+    f += 14 * pow(x, 13);
+    return f;
+
 }
 
-bool stopping_rule_1(number f3, number ro){
-    if(absolute(f3) < ro)
+bool stopping_rule_1(number x1, number x2, number ro){
+    if(absolute(x1 - x2) < ro)
         return true;
     return false;
 }
 
-bool stopping_rule_2(number x2, number x3, number ro){
-    if(absolute(x2 - x3) < ro)
+bool stopping_rule_2(number f, number ro){
+    if(absolute(f) < ro)
         return true;
     return false;
 }
 
-void secant_method(number a, number b, number ro, int stopping_rule_number){
-    cout << endl;
+void secant_method(number a, number b, number start1, number start2, number ro, int stopping_rule_number){
+    cout << endl << "Metoda siecznych\nx1 = " << start1 << "   x2 = " << start2 << "   ro = " << ro << endl;
 
     number x1, x2, x3, f1, f2, f3, tmp;
-    int number_of_iterations = 0, max_iters = 1000;
+    int number_of_iterations = 0, max_iters = 10000;
 
-    x1 = a;
-    x2 = b;
+    x1 = start1;
+    x2 = start2;
     f1 = f_function(x1);
     f2 = f_function(x2);
 
     while(number_of_iterations < max_iters){
-
-        if(absolute(f1 - f2) < 0.0000001){
-            cout << "zle punkty startowe - sytuacja z praktycznie taka sama wartoscia funkcji" << endl;
-        }
+        number_of_iterations++;
 
         x3 = x2;
         tmp = f2 * (x2 - x1);
@@ -59,48 +69,48 @@ void secant_method(number a, number b, number ro, int stopping_rule_number){
         x3 -= tmp;
         f3 = f_function(x3);
 
-        if(stopping_rule_number == 1 && stopping_rule_1(f3, ro))
+        if(stopping_rule_number == 1 && stopping_rule_1(x2, x3, ro))
             break;
 
-        if(stopping_rule_number == 2 && stopping_rule_2(x2, x3, ro))
+        if(stopping_rule_number == 2 && stopping_rule_2(f3, ro))
             break;
 
         x1 = x2;
         f1 = f2;
         x2 = x3;
         f2 = f3;
-        number_of_iterations++;
     }
 
-    cout << "liczba iteracji " << number_of_iterations << "\tmiejsce zerowe " << x3 << endl;
+    if(number_of_iterations == max_iters){
+        cout << "Przekroczono maksymalna liczbe iteracji w metodzie siecznych, prawdopodobnie zle dobrano punkty startowe" << endl;
+        return;
+    }
+
+    cout << "liczba iteracji " << number_of_iterations << "   miejsce zerowe " << x3 << "   delta " << proper_zero_of_f - x3 << endl;
 
 }
 
-void newtons_method(number a, number b, number ro, int stopping_rule_number){
-    cout << endl;
+void newtons_method(number a, number b, number start1, number ro, int stopping_rule_number){
+    cout << endl << "Metoda newtona\nx1 = " << start1 << "   ro = " << ro << endl;
 
     number x1, x2, f1, f_der_1, f2;
-    int number_of_iterations = 0, max_iters = 1000;
+    int number_of_iterations = 0, max_iters = 10000;
 
-    x1 = a;
+    x1 = start1;
     f1 = f_function(x1);
 
     while(number_of_iterations < max_iters){
 
         f_der_1 = f_derivative(x1);
 
-        if(absolute(f_der_1) < 0.0000001){
-            cout << "zly punkt startowy - zerowa pochodna" << endl;
-        }
-
         x2 = x1;
         x2 -= (f1 / f_der_1);
         f2 = f_function(x2);
 
-        if(stopping_rule_number == 1 && stopping_rule_1(f2, ro))
+        if(stopping_rule_number == 1 && stopping_rule_1(x1, x2, ro))
             break;
 
-        if(stopping_rule_number == 2 && stopping_rule_2(x1, x2, ro))
+        if(stopping_rule_number == 2 && stopping_rule_2(f1, ro))
             break;
 
         x1 = x2;
@@ -108,17 +118,51 @@ void newtons_method(number a, number b, number ro, int stopping_rule_number){
         number_of_iterations++;
     }
 
-    cout << "liczba iteracji " << number_of_iterations << "\tmiejsce zerowe " << x1 << endl;
+    if(number_of_iterations == max_iters){
+        cout << "Przekroczono maksymalna liczbe iteracji w metodzie newtona, prawdopodobnie zle dobrano punkty startowe" << endl;
+        return;
+    }
+
+    cout << "liczba iteracji " << number_of_iterations << "   miejsce zerowe " << x1 << "   delta " << proper_zero_of_f - x1 << endl;
 
 }
 
 int main(){
 
-    number a, b, ro;
+    number a, b, ro, start1, start2;
     int stopping_rule_number;
-    cout << "wpisz krance przedzialu, ro z warunku stopu i numer warunku stopu" << endl;
-    cin >> a >> b >> ro >> stopping_rule_number;
-    secant_method(a, b, ro, stopping_rule_number);
-    newtons_method(a, b, ro, stopping_rule_number);
+    // cout << "wpisz krance przedzialu, ro z warunku stopu i numer warunku stopu" << endl;
+    // cin >> a >> b >> ro >> stopping_rule_number;
+    // while(stopping_rule_number != 1 && stopping_rule_number != 2){
+    //     cout << "podaj numer warunku stopu - 1 lub 2" << endl;
+    //     cin >> stopping_rule_number;
+    // }
+
+    a = 0.2;
+    b = 2.5;
+    ro = 0.0000001;
+
+    for(stopping_rule_number = 1; stopping_rule_number <= 2; stopping_rule_number++){
+
+        cout<<endl<<endl<<endl<< "warunek stopu " << stopping_rule_number << endl;
+
+        for(start1 = b; start1 >= a; start1 -= 0.1){
+            newtons_method(a, b, start1, ro, stopping_rule_number);
+        }
+
+        cout<<endl<<endl<<endl;
+
+        start1 = a;
+        for(start2 = b; start2 > a + 0.01; start2 -= 0.1){
+            secant_method(a, b, start1, start2, ro, stopping_rule_number);
+        }
+
+        cout<<endl<<endl<<endl;
+
+        start1 = b;
+        for(start2 = a; start2 < b - 0.01; start2 += 0.1){
+            secant_method(a, b, start1, start2, ro, stopping_rule_number);
+        }
+    }
 
 }
