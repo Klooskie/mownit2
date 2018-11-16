b30aa309371e0b92ae9ef91206de26bce21df372
jakub <kuba.koniecznyxx@gmail.com>
2018-11-16 14:07:33 +0100

    solved second task

---
+++ lab3/lab3_zad1.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+using namespace std;
+typedef double number;
+
+
+number proper_zero_of_f = 0.522247;
+
+number absolute(number x){
+    return x < 0 ? (-1) * x : x;
+}
+
+//f(x) = (x - 1) * e ^ (-16 * x) + x ^ 14
+//f(x) = 0 dla x = 0.522247
+number f_function(number x){
+
+    number f = exp((-16) * x);
+    f *= (x - 1);
+    f += pow(x, 14);
+    return f;
+    
+}
+
+//f'(x) = 14 * x ^ 13 + (17 - 16x) * e ^ (-16x) 
+number f_derivative(number x){
+    
+    number f = exp((-16) * x);
+    f *= (17 - (16 * x));
+    f += 14 * pow(x, 13);
+    return f;
+
+}
+
+bool stopping_rule_1(number x1, number x2, number ro){
+    if(absolute(x1 - x2) < ro)
+        return true;
+    return false;
+}
+
+bool stopping_rule_2(number f, number ro){
+    if(absolute(f) < ro)
+        return true;
+    return false;
+}
+
+void secant_method(number a, number b, number start1, number start2, number ro, int stopping_rule_number){
+    cout << endl << "Metoda siecznych\nx1 = " << start1 << "   x2 = " << start2 << "   ro = " << ro << endl;
+
+    number x1, x2, x3, f1, f2, f3, tmp;
+    int number_of_iterations = 0, max_iters = 10000;
+
+    x1 = start1;
+    x2 = start2;
+    f1 = f_function(x1);
+    f2 = f_function(x2);
+
+    while(number_of_iterations < max_iters){
+        number_of_iterations++;
+
+        x3 = x2;
+        tmp = f2 * (x2 - x1);
+        tmp /= (f2 - f1);
+        x3 -= tmp;
+        f3 = f_function(x3);
+
+        if(stopping_rule_number == 1 && stopping_rule_1(x2, x3, ro))
+            break;
+
+        if(stopping_rule_number == 2 && stopping_rule_2(f3, ro))
+            break;
+
+        x1 = x2;
+        f1 = f2;
+        x2 = x3;
+        f2 = f3;
+    }
+
+    if(number_of_iterations == max_iters){
+        cout << "Przekroczono maksymalna liczbe iteracji w metodzie siecznych, prawdopodobnie zle dobrano punkty startowe" << endl;
+        return;
+    }
+
+    cout << "liczba iteracji " << number_of_iterations << "   miejsce zerowe " << x3 << "   delta " << proper_zero_of_f - x3 << endl;
+
+}
+
+void newtons_method(number a, number b, number start1, number ro, int stopping_rule_number){
+    cout << endl << "Metoda newtona\nx1 = " << start1 << "   ro = " << ro << endl;
+
+    number x1, x2, f1, f_der_1, f2;
+    int number_of_iterations = 0, max_iters = 10000;
+
+    x1 = start1;
+    f1 = f_function(x1);
+
+    while(number_of_iterations < max_iters){
+
+        f_der_1 = f_derivative(x1);
+
+        x2 = x1;
+        x2 -= (f1 / f_der_1);
+        f2 = f_function(x2);
+
+        if(stopping_rule_number == 1 && stopping_rule_1(x1, x2, ro))
+            break;
+
+        if(stopping_rule_number == 2 && stopping_rule_2(f1, ro))
+            break;
+
+        x1 = x2;
+        f1 = f2;
+        number_of_iterations++;
+    }
+
+    if(number_of_iterations == max_iters){
+        cout << "Przekroczono maksymalna liczbe iteracji w metodzie newtona, prawdopodobnie zle dobrano punkt startowy" << endl;
+        return;
+    }
+
+    cout << "liczba iteracji " << number_of_iterations << "   miejsce zerowe " << x1 << "   delta " << proper_zero_of_f - x1 << endl;
+
+}
+
+int main(){
+
+    number a, b, ro, start1, start2;
+    int stopping_rule_number;
+    a = 0.2;
+    b = 2.5;
+    ro = 0.0000001;
+
+    for(stopping_rule_number = 1; stopping_rule_number <= 2; stopping_rule_number++){
+
+        cout<< "\n\n\nwarunek stopu " << stopping_rule_number << endl;
+
+        for(start1 = b; start1 >= a; start1 -= 0.1){
+            newtons_method(a, b, start1, ro, stopping_rule_number);
+        }
+
+        cout << "\n\n\n";
+
+        start1 = a;
+        for(start2 = b; start2 > a + 0.01; start2 -= 0.1){
+            secant_method(a, b, start1, start2, ro, stopping_rule_number);
+        }
+
+        cout << "\n\n\n";
+
+        start1 = b;
+        for(start2 = a; start2 < b - 0.01; start2 += 0.1){
+            secant_method(a, b, start1, start2, ro, stopping_rule_number);
+        }
+    }
+
+}
---
+++ lab3/lab3_zad2.cpp
@@ -0,0 +1,251 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+using namespace std;
+typedef double number;
+
+
+number f1_fun(number x1, number x2, number x3){
+    return pow(x1, 2) + pow(x2, 2) + x3 - 1;
+}
+number f1_der_x1(number x1, number x2, number x3){
+    return 2 * x1;
+}
+number f1_der_x2(number x1, number x2, number x3){
+    return 2 * x2;
+}
+number f1_der_x3(number x1, number x2, number x3){
+    return 1;
+}
+
+number f2_fun(number x1, number x2, number x3){
+    return 2 * pow(x1, 2) + pow(x2, 2) + pow(x3, 3) - 2;
+}
+number f2_der_x1(number x1, number x2, number x3){
+    return 4 * x1;
+}
+number f2_der_x2(number x1, number x2, number x3){
+    return 2 * x2;
+}
+number f2_der_x3(number x1, number x2, number x3){
+    return 3 * pow(x3, 2);
+}
+
+number f3_fun(number x1, number x2, number x3){
+    return 3 * x1 - 2 * pow(x2, 3) - 2 * pow(x3, 2) - 3;
+}
+number f3_der_x1(number x1, number x2, number x3){
+    return 3;
+}
+number f3_der_x2(number x1, number x2, number x3){
+    return (-6) * pow(x2, 2);
+}
+number f3_der_x3(number x1, number x2, number x3){
+    return (-4) * x3;
+}
+
+void print_vector(number * x){
+    for(int i = 0; i < 3; i++)
+        cout << x[i] << endl;
+    cout << endl;
+}
+
+number absolute(number x){
+    return x < 0 ? (-1) * x : x;
+}
+
+number * multiply_matrix_and_vector(number ** matrix, number * vector){
+    number * result = new number [3];
+    for(int i = 0; i < 3; i++){
+        result[i] = 0;
+        for(int j = 0; j < 3; j++)
+            result[i] += matrix[i][j] * vector[j];
+    }
+    return result;
+}
+
+number * subtract_vectors(number * x1, number * x2){
+    number * result = new number [3];
+    for(int i = 0; i < 3; i++){
+        result[i] = x1[i] - x2[i];
+    }
+    return result;
+}
+
+number maximum_norm(number * x){
+    number max_number = 0;
+    for(int i = 0; i < 3; i++){
+        if(absolute(x[i]) > max_number)
+            max_number = absolute(x[i]);
+    }
+    return max_number;
+}
+
+number * podstaw_niewiadome(number * x){
+    number * result = new number [3];
+    result[0] = f1_fun(x[0], x[1], x[2]);
+    result[1] = f2_fun(x[0], x[1], x[2]);
+    result[2] = f3_fun(x[0], x[1], x[2]);
+    return result;
+}
+
+number * find_matching_proper_solution(number * x){
+    number * proper_solution_1 = new number [3];
+    number * proper_solution_2 = new number [3];
+    number * proper_solution_3 = new number [3];
+
+    proper_solution_1[0] = 1;
+    proper_solution_1[1] = -1;
+    proper_solution_1[2] = -1;
+
+    proper_solution_2[0] = 1;
+    proper_solution_2[1] = 0;
+    proper_solution_2[2] = 0;
+
+    proper_solution_3[0] = 0.953156;
+    proper_solution_3[1] = -0.428689;
+    proper_solution_3[2] = -0.0922802;
+
+    number diff_1 = maximum_norm(subtract_vectors(x, proper_solution_1));
+    number diff_2 = maximum_norm(subtract_vectors(x, proper_solution_2));
+    number diff_3 = maximum_norm(subtract_vectors(x, proper_solution_3));
+
+    if(diff_1 <= diff_2 && diff_1 <= diff_3)
+        return proper_solution_1;
+
+    if(diff_2 <= diff_3)
+        return proper_solution_2;
+
+    return proper_solution_3;
+}
+
+number * calculate_d(number * x){
+    
+    number * minus_f_podstawione = podstaw_niewiadome(x);
+    for(int i = 0; i < 3; i++)
+        minus_f_podstawione[i] *= -1;
+
+    number ** jacobian = new number * [3];
+    number ** inverse_jacobian = new number * [3];
+    for(int i = 0; i < 3; i++){
+        jacobian[i] = new number [3];
+        inverse_jacobian[i] = new number [3];
+    }
+
+    jacobian[0][0] = f1_der_x1(x[0], x[1], x[2]);
+    jacobian[0][1] = f1_der_x2(x[0], x[1], x[2]);
+    jacobian[0][2] = f1_der_x3(x[0], x[1], x[2]);
+
+    jacobian[1][0] = f2_der_x1(x[0], x[1], x[2]);
+    jacobian[1][1] = f2_der_x2(x[0], x[1], x[2]);
+    jacobian[1][2] = f2_der_x3(x[0], x[1], x[2]);
+
+    jacobian[2][0] = f3_der_x1(x[0], x[1], x[2]);
+    jacobian[2][1] = f3_der_x2(x[0], x[1], x[2]);
+    jacobian[2][2] = f3_der_x3(x[0], x[1], x[2]);
+
+    number determinant, inverse_determinant;
+
+    determinant = jacobian[0][0] * jacobian[1][1] * jacobian[2][2];
+    determinant += jacobian[1][0] * jacobian[2][1] * jacobian[0][2];
+    determinant += jacobian[2][0] + jacobian[0][1] * jacobian[1][2];
+    determinant -= jacobian[0][0] * jacobian[2][1] * jacobian[1][2];
+    determinant -= jacobian[2][0] * jacobian[1][1] * jacobian[0][2];
+    determinant -= jacobian[1][0] * jacobian[0][1] * jacobian[2][2];
+
+    inverse_determinant = 1 / determinant;
+
+    inverse_jacobian[0][0] = inverse_determinant * (jacobian[1][1] * jacobian[2][2] - jacobian[1][2] * jacobian[2][1]);
+    inverse_jacobian[0][1] = inverse_determinant * (jacobian[0][2] * jacobian[2][1] - jacobian[0][1] * jacobian[2][2]);
+    inverse_jacobian[0][2] = inverse_determinant * (jacobian[0][1] * jacobian[1][2] - jacobian[0][2] * jacobian[1][1]);
+    
+    inverse_jacobian[1][0] = inverse_determinant * (jacobian[1][2] * jacobian[2][0] - jacobian[1][0] * jacobian[2][2]);
+    inverse_jacobian[1][1] = inverse_determinant * (jacobian[0][0] * jacobian[2][2] - jacobian[0][2] * jacobian[2][0]);
+    inverse_jacobian[1][2] = inverse_determinant * (jacobian[0][2] * jacobian[1][0] - jacobian[0][0] * jacobian[1][2]);
+
+    inverse_jacobian[2][0] = inverse_determinant * (jacobian[1][0] * jacobian[2][1] - jacobian[1][1] * jacobian[2][0]);
+    inverse_jacobian[2][1] = inverse_determinant * (jacobian[0][1] * jacobian[2][0] - jacobian[0][0] * jacobian[2][1]);
+    inverse_jacobian[2][2] = inverse_determinant * (jacobian[0][0] * jacobian[1][1] - jacobian[0][1] * jacobian[1][0]);
+
+    return multiply_matrix_and_vector(inverse_jacobian, minus_f_podstawione);
+
+}
+
+bool stopping_rule_1(number * x1, number * x2, number ro){
+    if(maximum_norm(subtract_vectors(x1, x2)) < ro)
+        return true;
+    return false;
+}
+
+bool stopping_rule_2(number * x, number ro){
+    if(maximum_norm(podstaw_niewiadome(x)) < ro)
+        return true;
+    return false;
+}
+
+void newtons_method(number * start, number ro, int stopping_rule_number){
+    
+    number * x1 = start; 
+    number * x2 = new number [3];
+    number * d;
+
+    int number_of_iterations = 0, max_iters = 10000;
+
+    while(number_of_iterations < max_iters){
+        
+        // cout<<"x1"<<endl;
+        // print_vector(x1);
+
+        d = calculate_d(x1);
+
+        for(int i = 0; i < 3; i++)
+            x2[i] = x1[i] + d[i];
+
+        if(stopping_rule_number == 1 && stopping_rule_1(x1, x2, ro))
+            break;
+
+        if(stopping_rule_number == 2 && stopping_rule_2(x1, ro))
+            break;
+
+        free(d);
+        for(int i = 0; i < 3; i++)
+            x1[i] = x2[i];
+        number_of_iterations++;
+
+    }
+
+    if(number_of_iterations == max_iters){
+        cout << "Przekroczono maksymalna liczbe iteracji w metodzie newtona, prawdopodobnie zle dobrano punkt startowy" << endl;
+        return;
+    }
+
+    cout << "liczba iteracji " << number_of_iterations << endl;
+    cout << "wyliczony wektor:" << endl;
+    cout << "x1 = " << x1[0] << endl;
+    cout << "x2 = " << x1[1] << endl;
+    cout << "x3 = " << x1[2] << endl;
+
+    cout << "\nnajblizszy wyliczonemu wektor bedacy rozwiazaniem ukladu" << endl;
+    number * proper_solution = find_matching_proper_solution(x1);
+    print_vector(proper_solution);
+
+    cout << "delta " << maximum_norm(subtract_vectors(x1, proper_solution)) << endl;
+}
+
+int main(){
+
+    number * start = new number [3];
+    for(int i = 0; i < 3; i++)
+        start[i] = 1337;
+
+    number ro = 0.000001;
+    int stopping_rule_number = 2;
+
+    newtons_method(start, ro, stopping_rule_number);
+
+}
