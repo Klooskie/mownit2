0719e5f468ede05f079507028b43a779f0ee7f87
jakub <kuba.koniecznyxx@gmail.com>
2019-01-09 01:40:27 +0100

    add first version of programs solving ODEs

---
+++ lab7/first_order_differential_equations.py
@@ -0,0 +1,147 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from math import sin, cos, pi, exp
+
+
+def maximum_norm(vector):
+    m = 0
+    for x in vector:
+        if abs(x) > m:
+            m = abs(x)
+    return m
+
+
+def y_derivative(x, y):
+    m = 5
+    k = 10
+    return y
+
+
+def solution(x):
+    m = 5
+    k = 10
+    return exp(x)
+
+
+def solution_for_domain(domain):
+    result = []
+    for x in domain:
+        result.append(solution(x))
+    return result
+
+
+def euler_method(x, points, n):
+    i = 0
+    while x > points[i + 1][0]:
+        if i + 1 < n:
+            i += 1
+        else:
+            break
+
+    a = (points[i + 1][1] - points[i][1]) / (points[i + 1][0] - points[i][0])
+    b = points[i][1] - a * points[i][0]
+    result = a * x + b
+
+    return result
+
+
+def euler_method_for_domain(domain, n, x_0, x_k, a):
+    result = []
+    difference = []
+
+    step = (x_k - x_0) / n
+
+    points = [(x_0, a)]
+    x = x_0
+    y = a
+    for i in range(n):
+        y = y + step * y_derivative(x, y)
+        x = x + step
+        points.append((x, y))
+
+    for point in domain:
+        result.append(euler_method(point, points, n))
+        difference.append(solution(point) - result[-1])
+
+    print("norma maksimum roznicy dokladnego rozwiazania i rozwiazania wyliczonego metoda Eulera\n"
+          + "dla kroku rownego " + str(step) + " (podzial przedzialu na " + str(n) + " czesci):\n"
+          + str(maximum_norm(difference)) + "\n")
+    return result
+
+
+def runge_kutta_method(x, points, n):
+    i = 0
+    while x > points[i + 1][0]:
+        if i + 1 < n:
+            i += 1
+        else:
+            break
+
+    a = (points[i + 1][1] - points[i][1]) / (points[i + 1][0] - points[i][0])
+    b = points[i][1] - a * points[i][0]
+    result = a * x + b
+
+    return result
+
+
+def runge_kutta_method_for_domain(domain, n, x_0, x_k, a):
+    result = []
+    difference = []
+
+    step = (x_k - x_0) / n
+
+    points = [(x_0, a)]
+    x = x_0
+    y = a
+    for i in range(n):
+        k1 = step * y_derivative(x, y)
+        k2 = step * y_derivative(x + step / 2, y + k1 / 2)
+        k3 = step * y_derivative(x + step / 2, y + k2 / 2)
+        k4 = step * y_derivative(x + step, y + k3)
+        y = y + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        x = x + step
+        points.append((x, y))
+
+    for point in domain:
+        result.append(euler_method(point, points, n))
+        difference.append(solution(point) - result[-1])
+
+    print("norma maksimum roznicy dokladnego rozwiazania i rozwiazania wyliczonego metoda Rungego-Kutty 4. stopnia\n"
+          + "dla kroku rownego " + str(step) + " (podzial przedzialu na " + str(n) + " czesci):\n"
+          + str(maximum_norm(difference)))
+    return result
+
+
+def main():
+    # liczba krokow na przedziale [x_0, x_k]
+    n = 10
+
+    x_0 = 0
+    x_k = 4
+    a = solution(x_0)
+
+    domain = np.linspace(x_0, x_k, num=1500)
+
+    fig = plt.figure(1)
+    fig.canvas.set_window_title('Rozwiazania rownania rozniczkowego otrzymane roznymi metodami')
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.subplot(211)
+    plt.grid(True)
+    plt.grid(True)
+    plt.plot(domain, euler_method_for_domain(domain, n, x_0, x_k, a), 'k-')
+    plt.plot(domain, solution_for_domain(domain), 'b--')
+    plt.title('y(x) uzyskane za pomoca metody Eulera')
+
+    plt.subplot(212)
+    plt.grid(True)
+    plt.plot(domain, runge_kutta_method_for_domain(domain, n, x_0, x_k, a), 'k-')
+    plt.plot(domain, solution_for_domain(domain), 'b--')
+    plt.title('y(x) uzyskane za pomoca metody Rungego-Kutty 4. stopnia')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab7/second_order_differential_equations.py
@@ -0,0 +1,147 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from math import sin, cos, pi, exp
+
+
+def maximum_norm(vector):
+    m = 0
+    for x in vector:
+        if abs(x) > m:
+            m = abs(x)
+    return m
+
+
+def solution(x):
+    m = 5
+    k = 10
+    # return exp(x)
+    return 0
+
+
+def solution_for_domain(domain):
+    result = []
+    for x in domain:
+        result.append(solution(x))
+    return result
+
+
+def finite_difference_method(x, points, n):
+    i = 0
+    while x > points[i + 1][0]:
+        if i + 1 < n:
+            i += 1
+        else:
+            break
+
+    a = (points[i + 1][1] - points[i][1]) / (points[i + 1][0] - points[i][0])
+    b = points[i][1] - a * points[i][0]
+    result = a * x + b
+
+    return result
+
+
+def finite_difference_method_for_domain(domain, n, x_0, x_k, a, b):
+    result = []
+    difference = []
+
+    # obliczenie kroku
+    step = (x_k - x_0) / n
+
+    # wypelnienie listy kolejnych x
+    x_col = []
+    for i in range(n):
+        x_col.append(x_0 + i * step)
+    x_col.append(x_k)
+
+    # stworzenie 3 kolumn macierzy trojdiagonalnej ukladu rownan
+    col_1 = []
+    col_2 = []
+    col_3 = []
+
+    # wypelnienie macierzy trojdiagonalnej w zaleznosci od podanego rownania kolejno:
+    # col_1 wspolczynnikami przy y_i-1
+    # col_2 wspolczynnikami przy y_i
+    # col_3 wspolczynnikami przy y_i+1
+    for i in range(1, n):
+        col_1.append(1 / (1.5 ** 2))
+        col_2.append((-2) / (1.5 ** 2) - (1 / x_col[i]) * (1 / 1.5) - (1 / x_col[i] ** 2))
+        col_3.append(1 / (1.5 ** 2) + (1 / x_col[i]) * (1 / 1.5))
+
+    # algorytm Thomasa
+    # obliczenie wspolczynnikow B_0 i Y_0
+    b_factors = [-col_3[0] / col_2[0]]
+    y_factors = [-a * col_1[0] / col_2[0]]
+
+    # obliczenie kolejnych wspolczynnikow B i Y
+    for i in range(1, n - 2):
+        new_b_factor = -col_3[i]
+        new_b_factor /= (col_1[i] * b_factors[-1] + col_2[i])
+
+        new_y_factor = 0 - col_1[i] * y_factors[-1]
+        new_y_factor /= (col_1[i] * b_factors[-1] + col_2[i])
+
+        b_factors.append(new_b_factor)
+        y_factors.append(new_y_factor)
+
+    # dodanie do wyniku y(x_k)
+    y_col = [b]
+
+    # obliczenie i dodanie do wyniku y(x_k-1)
+    last_x = (-b * col_3[n - 2]) - col_1[n - 2] * y_factors[-1]
+    last_x /= (col_1[n - 2] * b_factors[-1] + col_2[n - 2])
+    y_col.append(last_x)
+
+    # obliczenie od konca kolejnych wartosci y(x)
+    for i in range(n - 3, -1, -1):
+        y_col.append(b_factors[i] * y_col[-1] + y_factors[i])
+
+    # dodanie do wyniku y(x_0) i odwrocenie go
+    y_col.append(a)
+    y_col.reverse()
+
+    # stworzenie listy krotek z obliczonymi koleinymi x i y(x)
+    points = []
+    for i in range(n + 1):
+        points.append((x_col[i], y_col[i]))
+    # print(points)
+
+    for point in domain:
+        result.append(finite_difference_method(point, points, n))
+        difference.append(solution(point) - result[-1])
+
+    print("norma maksimum roznicy dokladnego rozwiazania i rozwiazania wyliczonego metoda roznic skonczonych\n"
+          + "dla kroku rownego " + str(step) + " (podzial przedzialu na " + str(n) + " czesci):\n"
+          + str(maximum_norm(difference)))
+    return result
+
+
+def main():
+    # liczba krokow na przedziale [x_0, x_k]
+    n = 3
+
+    x_0 = 2
+    x_k = 6.5
+    # a = solution(x_0)
+    # b = solution(x_k)
+    a = 0.008
+    b = 0.003
+
+    domain = np.linspace(x_0, x_k, num=1500)
+
+    fig = plt.figure(1)
+    fig.canvas.set_window_title('Rozwiazanie rownania rozniczkowego drugiego stopnia')
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.subplot(111)
+    plt.grid(True)
+    plt.grid(True)
+    plt.plot(domain, finite_difference_method_for_domain(domain, n, x_0, x_k, a, b), 'k-')
+    plt.plot(domain, solution_for_domain(domain), 'b--')
+    plt.title('y(x) uzyskane za pomoca metody roznic skonczonych')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
